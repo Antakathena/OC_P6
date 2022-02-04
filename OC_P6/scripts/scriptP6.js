42b96d8f45cc248ae6d8dc11157b35374889d05f
Antakathena <s.trebatius@gmail.com>
2022-02-04 16:06:02 +0100

    site fonctionnel

--- OC_P6/scripts/scriptP6.js
+++ OC_P6/scripts/scriptP6.js
@@ -4,8 +4,11 @@
 
 // Ici on récupère le json du meilleur film
 window.onload = () => {
-    var data = {};
-    var meilleurFilm = null
+
+    const articleMeilleurFilm = document.getElementById("article-meilleur-film")
+    const modal = document.getElementById("myModal");
+    // Get the button that opens the modal
+    const btn = document.getElementById("myBtn");
 
     // function SetMeilleurFilm(data) {
     //     // c'est ici qu'on modifie l'image de la balise du meilleur film
@@ -23,6 +26,7 @@ window.onload = () => {
     const meilleurFilmImg = document.getElementById("img1")
     const titre1 = document.getElementById("titre1")
     const resume1 = document.getElementById("résumé1")
+    const infosModale1 = modal.getElementsByTagName("dd")
 
     fetch("http://localhost:8000/api/v1/titles?sort_by=-imdb_score")
         .then(reponse => {
@@ -49,24 +53,70 @@ window.onload = () => {
             }
         })
         .then(reponse2 => {
-            resume1.innerText = reponse2.description
+            var datas = [];
+            console.log(reponse2)
+            datas.push(
+                reponse2.image_url, reponse2.title, reponse2.genres, reponse2.date_published,
+                reponse2.rated, reponse2.imdb_score, reponse2.directors, reponse2.actors,
+                reponse2.duration, reponse2.countries, reponse2.avg_vote, reponse2.description)
+            for (i = 0; i < datas.length; i++) {
+                infosModale1[i].innerText = datas[i]
+                resume1.innerText = reponse2.description
+            }
         })
 
     // carroussel
-    var fonctionCarroussel = function carroussel(article, carroussel, urlImages) {
+    // var filmsIDs = []
+    // console.log(filmsIDs)
+
+    var peuplerModale = function peuplerModale(filmId) {
+        fetch(`http://localhost:8000/api/v1/titles/` + filmId.toString())
+            .then(reponse => {
+                if (reponse.ok) {
+                    return reponse.json()
+                } else {
+                    console.log("ERREUR");
+                }
+            })
+            .then(reponse2 => {
+                console.log(reponse2)
+                var datas = [];
+                datas.push(
+                    reponse2.image_url, reponse2.title, reponse2.genres, reponse2.date_published,
+                    reponse2.rated, reponse2.imdb_score, reponse2.directors, reponse2.actors,
+                    reponse2.duration, reponse2.countries, reponse2.avg_vote, reponse2.description)
+                for (i = 0; i < datas.length; i++) {
+                    infosModale1[i].innerText = datas[i]
+                }
+            })
+    }
+
+    var fonctionCarroussel = function carroussel(article, carroussel, imageData) {
+        // urlimages: [ID, URL]
+        console.log(imageData);
         nbr = 10; /*nombre d'images dans le carroussel*/
         p = 0; /*position par défaut*/
         g = article.getElementsByClassName("g")[0];
         d = article.getElementsByClassName("d")[0];
         // il faut rajouter à droite les 3 premières images de gauche:
-        let double123 = urlImages.slice(0, 3);
-        let images10 = urlImages.concat(double123);
+        let double123 = imageData.slice(0, 3);
+        let images10 = imageData.concat(double123);
         // pour chaque url on créé une image :
-        images10.forEach(urlImage => {
+        images10.forEach(imageData => {
             img = document.createElement("img"); /*on créé les img dynamiquement*/
             img.className = "photo";
-            img.src = urlImage;
-            carroussel.appendChild(img); /*on ajoute les nouvelles img au container principal*/
+            img.src = imageData[1];
+            let idImg = imageData[0];
+            bouton = document.createElement("button");
+            bouton.onclick = function () {
+                console.log("Chargement modale " + idImg);
+                peuplerModale(idImg);
+                modal.style.display = "block";
+            }
+
+            bouton.appendChild(img);
+            // ... créer le bouton qui va utiliser urlData[0] (l'id), et mapper son onclick à peuplerModale
+            carroussel.appendChild(bouton); /*on ajoute les nouvelles img au container principal*/
         })
         d.onclick = function () {
             if (p > -3) { /*arrête l'animation du carroussel pour arrêter quand il n'y a plus d'images*/
@@ -86,22 +136,29 @@ window.onload = () => {
             carroussel.style.transform = "translate(" + p * 199 + "px)";
         }
     }
+
     function get7films(urlAPI, article, carroussel, fonctionCarroussel) {
         fetch(urlAPI)
             .then(reponse => reponse.json())
             .then(reponse2 => {
-                // console.log(reponse2)
+                console.log(reponse2) // là on a les id de tous dans la liste 0-6 => à récup pour modales
+                // reponse2.results.forEach(element => filmsIDs.push(element.id));
                 return reponse2
             })
             .then(reponse2 => {// pour chaque element(film) dans le json, récupérer l'image
-                urlImages = [];
-                reponse2.results.forEach(element => urlImages.push(element.image_url));
+                imageData = [];
+                reponse2.results.forEach(element => imageData.push([element.id, element.image_url]));
                 // console.log(urlImages);
-                return urlImages;
+                return imageData;
             })
-            .then(urlImages => fonctionCarroussel(article, carroussel, urlImages))
+            .then(imageData => fonctionCarroussel(article, carroussel, imageData))
         // fonction carroussel qui transforme les images en carroussel
     }
+
+    // const listeArticle = document.getElementsByClassName("catégorie")
+    // const listeCarroussel = document.getElementsByClassName("carroussel")
+    // const listeUrlCategorie= [liste des url en dur]
+    // for(i=0; i<4; i++) faire comme plus haut
 
     var article1 = document.getElementById("mieux-notés");
     var carroussel1 = document.getElementById('carroussel1');
@@ -136,14 +193,8 @@ window.onload = () => {
         carroussel4,
         fonctionCarroussel)
 
-    //modal
 
-
-    // Get the modal
-    var modal = document.getElementById("myModal");
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+    //modales
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close");
@@ -164,5 +215,4 @@ window.onload = () => {
             modal.style.display = "none";
         }
     }
-
 }
